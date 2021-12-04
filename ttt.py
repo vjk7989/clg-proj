974f6588d5c27d2dec05238dccbbbbce1f1bb308
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2021-12-05 02:14:20 +0530

    fixed a small bug 😅

--- ttt.py
+++ ttt.py
@@ -129,7 +129,7 @@ def main():
 
 
 while True:
-    answer = input('Do you want to play again? (Y/N)')
+    answer = input('Do you want to play ? (Y/N)')
     if answer.lower() == 'y' or answer.lower == 'yes':
         board = [' ' for x in range(10)]
         print('-----------------------------------')
