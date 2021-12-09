5d10cd2ec4fbc542d34bd864d527c77423c887e5
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2021-12-10 02:51:37 +0530

    added user input

--- tsler.py
+++ tsler.py
@@ -4,8 +4,13 @@ from plyer import notification
 import time
 from plyer import notification
 
+
+
 if __name__ == "__main__":
-    while True:
+    '''
+    run = False
+    
+    while run:
 
         notification.notify(
             title="hey!!!",
@@ -16,4 +21,22 @@ if __name__ == "__main__":
             toast = True
         )
     # waiting time
-        time.sleep(9)
+        time.sleep(9)'''
+    Title= input("Enter the title : ")
+    Message = input("Enter the message : ")
+    Appr = int(input("Enter time in sec to show the message : "))
+    print("\nmake it 0 if not required \n")
+    Hour = int(input("Enter number of hours the message to appear in : "))
+    miN = int(input("Enter number of minutes the message to appear in : "))
+    sec = int(input("Enter number of second the message to appear in : "))
+    timE_sleep = Hour*60*60 + miN*60 + sec
+    while True :
+        notification.notify(
+            title= Title,
+            message = Message,
+            app_icon="D:\pythonProject\clg-proj\He.ico",
+            timeout=Appr,
+            ticker="Vit ap",
+            toast=True
+        )
+        time.sleep(timE_sleep)
