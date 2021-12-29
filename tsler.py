b4155f82ed19b7b98d34f50af2fd961a37b31cc9
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2021-12-30 00:56:51 +0530

    vol

--- tsler.py
+++ tsler.py
@@ -1,5 +1,5 @@
 import time
-from plyer import notification
+from plyer import notification #pip install plyer
 
 import time
 from plyer import notification
@@ -22,13 +22,13 @@ if __name__ == "__main__":
         )
     # waiting time
         time.sleep(9)'''
-    Title= input("Enter the title : ")
-    Message = input("Enter the message : ")
-    Appr = int(input("Enter time in sec to show the message : "))
+    Title= "Break"#input("Enter the title : ")
+    Message = "Take a break " #input("Enter the message : ")
+    Appr =  10 #int(input("Enter time in sec to show the message : "))
     print("\nmake it 0 if not required \n")
-    Hour = int(input("Enter number of hours the message to appear in : "))
-    miN = int(input("Enter number of minutes the message to appear in : "))
-    sec = int(input("Enter number of second the message to appear in : "))
+    Hour = 0#int(input("Enter number of hours the message to appear in : "))
+    miN = 30#int(input("Enter number of minutes the message to appear in : "))
+    sec = 6#int(input("Enter number of second the message to appear in : "))
     timE_sleep = Hour*60*60 + miN*60 + sec
     while True :
         notification.notify(
@@ -40,3 +40,4 @@ if __name__ == "__main__":
             toast=True
         )
         time.sleep(timE_sleep)
+
